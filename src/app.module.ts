f6896d401349c0769dc1c24830f2e8809fb79d1a
Rodrigo Coelho <rodrigo.coelho@hotmail.com.br>
2023-06-16 16:34:03 -0400

    feat: add group to app routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { BusinessUnitsModule } from "./modules/businessUnits/businessUnits.module";
+import { GroupsModule } from "./modules/groups/groups.module";
 
 @Module({
-  imports: [BusinessUnitsModule],
+  imports: [BusinessUnitsModule, GroupsModule],
 })
 export class AppModule {}
